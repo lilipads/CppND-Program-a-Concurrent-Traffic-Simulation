1ad9f7f59245384038329f704fd9802bfa9bf3a9
Lili Jiang <lilijiang15@gmail.com>
2020-05-14 22:23:18 -0500

    done - task 4: message queue send()

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -20,6 +20,9 @@ void MessageQueue<T>::send(T &&msg) {
   // std::lock_guard<std::mutex>
   // as well as _condition.notify_one() to add a new message to the queue and
   // afterwards send a notification.
+  std::lock_guard<std::mutex> lock(_mutex);
+  _queue.push_back(std::move(msg));
+  _condition.notify_one();
 }
 
 /* Implementation of class "TrafficLight" */
